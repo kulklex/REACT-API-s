338f19d81eabe6066033972f56c139cfe8f92a55
kulklex <kulklex@gmail.com>
2023-02-20 00:25:56 +0100

    Setting up images maxHeight

--- src/components/imageCard.js
+++ src/components/imageCard.js
@@ -3,10 +3,9 @@ import React from "react";
 const ImageCard = ({ image, searchText }) => {
   const tags = image?.tags.split(",");
 
-
   return (
     <div className=" max-w-sm rounded overflow-hidden shadow-lg">
-      <img src={image?.webformatURL} alt="" className="w-full" />
+      <img style={{maxHeight: 250, height:250}} src={image?.webformatURL} alt="" className="w-full" />
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl mb-2">
           Photo by {image?.user}
